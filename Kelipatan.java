62f7b2af3a0459fcab0d23cbba0a997c44e53ea1
gianprakoso18 <gianprakoso60@gmail.com>
2024-11-04 18:39:15 +0700

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kelipatan;
+
+/**
+ *
+ * @author gianp
+ * NAMA     : Gian Prakoso
+ * KELAS    : Sistem Informasi
+ * NIM      : 23176034
+ */
+public class Kelipatan {
+    public static void main(String[] args) {
+        double angka = 3.5;
+        while (angka <= 35) {
+            System.out.println(angka);
+            angka += 3.5;
+        }
+    }
+}
